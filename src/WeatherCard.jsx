448040a7b37be358d19bf73104ea600c82768f3d
idris <idrisdodwell00711@gmail.com>
2020-09-24 18:14:51 +0100

    styling on WeatherCard

--- src/WeatherCard.jsx
+++ src/WeatherCard.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+
+let ScreenWidth = Dimensions.get("window").width;
+let ScreenHeight = Dimensions.get("window").height;
+
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:"center",
+        display:'flex',
+        flex: 1,
+    padding: 8,
+    margin:8
+    },
+ 
+    text:{
+        flex: 0.3,
+    
+        color: "white",
+    textShadowColor: "blue",
+    textShadowRadius: 1,
+    margin: 44,
+    textAlign:'center',
+    paddingLeft:40,
+    fontFamily:'comic sans'
+
+
+    },
+    box:{
+       flex:1,
+       justifyContent:"center",
+        backgroundColor: "deepskyblue",
+        borderWidth: 3,
+        borderStartColor:'grey',
+        opacity:0.7,
+        alignItems:'center',
+        flexDirection:'column',
+        borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    margin: '10%',
+    marginBottom:'15%',
+    marginBottom:'15%',
+    marginLeft:'30%',
+    marginRight:'30%',
+    padding:10
+
+    }
+})
+
+//let ScreenWidth = Dimensions.get("window").width;
 
 export default function WeatherCard(props){
+    console.log('weatherCard')
       const { 
           temp,humidity, pressure, name, visibility, id:weatherId, description, icon, speed:windSpeed, country, sunrise, sunset
         } = props.route.params;
@@ -10,14 +61,41 @@ export default function WeatherCard(props){
     let tempx = Math.round( temp-273, 2)
 
     
-   return ( <View>
-        <Text>
-        {name}
-        {country}
-        {description}
-        {windSpeed}
-        itemId: {tempx+'c'} 
-        humid:1
+   return ( <View style={styles.container}>
+    <LinearGradient
+        // Background Linear Gradient
+        colors={['#171e26', '#3f586b']}
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 0,
+          height: ScreenHeight
+        }}
+      />
+      <View style={styles.box}>
+          
+          <View style={styles.text}>
+
+          <LinearGradient  colors={['#4c669f', '#3b5998', '#192f6a']}
+        style={{ padding: 5, alignItems: 'center', borderRadius: 2 }}>
+
+        <Text style={styles.text} >
+        {name } 
+       {tempx+'c'} 
         </Text>
+        <Text style={styles.text}>
+        {country}
+        </Text>
+        <Text style={styles.text}>
+        {description}
+        </Text>
+        <Text style={styles.text}>
+        {windSpeed}
+        </Text>
+          </LinearGradient>
+
+          </View>
+      </View>
     </View>)
 }
